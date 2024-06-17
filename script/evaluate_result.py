c56c1a0d28d3d0bf1f4f14c32f4c7039dca22eb1
allen <yuyalun.allen@gmail.com>
2024-06-17 11:34:03 +0800

    Sync

--- script/evaluate_result.py
+++ script/evaluate_result.py
@@ -1,7 +1,6 @@
 import os
 import time
 import json
-from functools import partial
 
 import torch
 from tqdm import tqdm
@@ -9,77 +8,38 @@ import fire
 from transformers import (
     AutoTokenizer,
     LlamaForCausalLM,
+    BitsAndBytesConfig
 )
 from peft import PeftModel
 from codebleu import calc_codebleu
-from transformers import (
-    DPRContextEncoder,
-    DPRContextEncoderTokenizerFast,
-    HfArgumentParser,
-    RagRetriever,
-    RagSequenceForGeneration,
-    RagTokenizer,
-)
-from datasets import Features, Sequence, Value, load_dataset
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 
 from generate_dataset import load_merge_conflict_and_resolution_chunk
 
 
-def embed(documents: dict, ctx_encoder: DPRContextEncoder, ctx_tokenizer: DPRContextEncoderTokenizerFast) -> dict:
-    """Compute the DPR embeddings of document passages"""
-    input_ids = ctx_tokenizer(
-        documents["conflict"], documents["resolution"], truncation=True, padding="longest", return_tensors="pt"
-    )["input_ids"]
-    embeddings = ctx_encoder(input_ids.to("cuda"), return_dict=True).pooler_output
-    return {"embeddings": embeddings.detach().cpu().numpy()}
-
-
 def main(**kwargs):
-    # ctx_encoder = DPRContextEncoder.from_pretrained(rag_example_args.dpr_ctx_encoder_model_name).to("cuda")
-    # ctx_tokenizer = DPRContextEncoderTokenizerFast.from_pretrained(rag_example_args.dpr_ctx_encoder_model_name)
-    # new_features = Features(
-    #     {"conflict": Value("string"), "resolution": Value("string"), "embeddings": Sequence(Value("float32"))}
-    # )  # optional, save as float32 instead of float64 to save space
-    # dataset = dataset.map(
-    #     partial(embed, ctx_encoder=ctx_encoder, ctx_tokenizer=ctx_tokenizer),
-    #     batched=True,
-    #     batch_size=processing_args.batch_size,
-    #     features=new_features,
-    # )
+    with open("filtered_chunks.json", "r") as f:
+        c_n_r_chunks  = json.load(f)
+    c_n_r_chunks_index = c_n_r_chunks[: int(len(c_n_r_chunks)*0.8)]
+    c_n_r_chunks_eval = c_n_r_chunks[int(len(c_n_r_chunks)*0.999) :]
 
-    # # And finally save your dataset
-    # passages_path = os.path.join(rag_example_args.output_dir, "my_knowledge_dataset")
-    # dataset.save_to_disk(passages_path)
-    # # from datasets import load_from_disk
-    # # dataset = load_from_disk(passages_path)  # to reload the dataset
+    ST = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
+    chunks_embeddings = ST.encode(c_n_r_chunks_index)
+    chunks_embeddings = np.array(chunks_embeddings).astype('float32')
+    index = faiss.IndexFlatL2(chunks_embeddings.shape[1])
+    index.add(chunks_embeddings)
 
-    # ######################################
-    # logger.info("Step 2 - Index the dataset")
-    # ######################################
+    # use quantization to lower GPU usage
+    bnb_config = BitsAndBytesConfig(
+        load_in_8bit=True
+    )
 
-    # # Let's use the Faiss implementation of HNSW for fast approximate nearest neighbor search
-    # index = faiss.IndexHNSWFlat(index_hnsw_args.d, index_hnsw_args.m, faiss.METRIC_INNER_PRODUCT)
-    # dataset.add_faiss_index("embeddings", custom_index=index)
-
-    # # And save the index
-    # index_path = os.path.join(rag_example_args.output_dir, "my_knowledge_dataset_hnsw_index.faiss")
-    # dataset.get_index("embeddings").save(index_path)
-    # # dataset.load_faiss_index("embeddings", index_path)  # to reload the index
-
-    # ######################################
-    # logger.info("Step 3 - Load RAG")
-    # ######################################
-
-    # # Easy way to load the model
-    # retriever = RagRetriever.from_pretrained(
-    #     rag_example_args.rag_model_name, index_name="custom", indexed_dataset=dataset
-    # )
-    # model = RagSequenceForGeneration.from_pretrained(rag_example_args.rag_model_name, retriever=retriever)
-    # tokenizer = RagTokenizer.from_pretrained(rag_example_args.rag_model_name)
     torch.cuda.manual_seed(42)
     model = LlamaForCausalLM.from_pretrained(
         kwargs["model_name"],
-        load_in_8bit=True,
+        quantization_config=bnb_config,
         device_map="auto",
         return_dict=True,
     )
@@ -94,13 +54,16 @@ def main(**kwargs):
         print("WARNING: Resizing the embedding matrix to match the tokenizer vocab size.")
         model.resize_token_embeddings(len(tokenizer))
 
-    with open("filtered_chunks.json", "r") as f:
-        c_n_r_chunks  = json.load(f)
-    c_n_r_chunks = c_n_r_chunks[int(len(c_n_r_chunks)*0.999) :]
     # c_n_r_chunks = c_n_r_chunks[:int(len(c_n_r_chunks)*0.001)]
-    evaluation(model=model, eval_data=c_n_r_chunks, tokenizer=tokenizer)
+    evaluation(model=model, eval_data=c_n_r_chunks_eval, tokenizer=tokenizer)
 
-def evaluation(model, eval_data, tokenizer):
+def retrieve_docs(query, retriever_model, index, corpus_texts, top_k=5):
+    query_embedding = retriever_model.encode([query]).astype('float32')
+    _, indices = index.search(query_embedding, top_k)
+    return [corpus_texts[idx] for idx in indices[0]]
+
+
+def evaluation(model, eval_data, tokenizer, retriever_model, index, corpus_text):
     """
     Evaluates the model on the given dataloader
 
@@ -114,8 +77,11 @@ def evaluation(model, eval_data, tokenizer):
     model.eval()
     os.makedirs("results", exist_ok=True)
     for id, batch in enumerate(tqdm(eval_data,colour="green", desc="evaluating Epoch", dynamic_ncols=True)):
+        SYS_PROMPT = """You are an assistant for resolving merge conflicts with given conflict chunks.
+        You are first given an unresolved conflict chunk following with several resolution examples. Provide the resolution of the conflict chunk.
+        Only output the resolution and do not output any explanation."""
         prompt = (f"""
-Output the the resolution code of this merge conflict without any explanation:
+Conflict:
 
 ```
 {batch['conflict']}
@@ -123,16 +89,21 @@ Output the the resolution code of this merge conflict without any explanation:
 
 Resolution:
 
-```
         """)
-        prompt_ids = tokenizer(prompt, return_tensors="pt")
-        prompt_ids = {k: v.to("cuda") for k, v in prompt_ids.items()}
+        _ , retrieved_documents = retrieve_docs(prompt, retriever_model, index, corpus_text, 5)
+        messages = [{"role":"system","content":SYS_PROMPT},{"role":"user","content":prompt}]
+        # tell the model to generate
+        input_ids = tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt"
+            ).to(model.device)
         codebleu_score = []
 
         start = time.perf_counter()
         with torch.no_grad():
             outputs = model.generate(
-                **prompt_ids,
+                input_ids,
                 max_new_tokens=256,
                 do_sample=True,
                 top_p=1.0,
@@ -145,11 +116,17 @@ Resolution:
         e2e_inference_time = (time.perf_counter()-start)*1000
         print(f"the inference time is {e2e_inference_time} ms")
         # Only get the output text without the prompt text
-        output_text = tokenizer.decode(outputs[0][torch.sum(prompt_ids['attention_mask']).item()+1:], skip_special_tokens=True)
-        codebleu_result = calc_codebleu([batch['resolution']], [output_text], lang="java", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=tokenizer)
+        # output_text = tokenizer.decode(outputs[0][torch.sum(input_ids['attention_mask']).item()+1:], skip_special_tokens=True)
+        output_text: str = tokenizer.batch_decode(outputs[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
+        resolution = output_text.split('\n')
+        if len(resolution) > 2:
+            resolution = '\n'.join(resolution[1:-1])
+        else:
+            resolution = ''
+        codebleu_result = calc_codebleu([batch['resolution']], [resolution], lang="java", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=tokenizer)
         codebleu_score.append(codebleu_result['codebleu'])
         with open(f"results/{id}.txt", "w") as f:
-            f.write(f"######Resolution output:\n{output_text}\n######Reference resolution\n{batch['resolution']}\n######codebleu result:\n{codebleu_result}")
+            f.write(f"######Resolution output:\n{resolution}\n######Reference resolution\n{batch['resolution']}\n######codebleu result:\n{codebleu_result}")
         print(codebleu_result['codebleu'])
         
     with open(f"results/average_codebleu.txt", "w") as f:
