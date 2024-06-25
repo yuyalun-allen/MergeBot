f5942bf6e553bbe914968258a8c9b80c645d0798
allen <yuyalun.allen@gmail.com>
2024-06-25 15:47:04 +0800

    Implement RAG

--- script/evaluate_result.py
+++ script/evaluate_result.py
@@ -14,7 +14,12 @@ from transformers import (
 from peft import PeftModel
 from codebleu import calc_codebleu
 from sentence_transformers import SentenceTransformer
-from datasets import Dataset
+from datasets import (
+    Dataset,
+    Features,
+    Value,
+    Sequence
+)
 
 
 def embed(chunk: dict, model: SentenceTransformer) -> dict:
@@ -27,22 +32,30 @@ def embed(chunk: dict, model: SentenceTransformer) -> dict:
     return {
         "conflict": chunk["conflict"],
         "resolution": chunk["resolution"],
-        "embeddings": embeddings.cpu().numpy()
+        "embeddings": embeddings.cpu().numpy()[0]
     }
 
 
-def create_index(ST: SentenceTransformer):
+def create_embedding_index(ST: SentenceTransformer):
     with open("filtered_chunks.json", "r") as f:
         c_n_r_chunks  = json.load(f)
     c_n_r_chunks_index = c_n_r_chunks[: int(len(c_n_r_chunks)*0.8)]
     chunks_dataset = Dataset.from_list(c_n_r_chunks_index)
-    chunks_dataset.map(
+    new_features = Features(
+        {"conflict": Value("string"), "resolution": Value("string"), "embeddings": Sequence(Value("float32"))}
+    )  # optional, save as float32 instead of float64 to save space
+    chunks_dataset = chunks_dataset.map(
         partial(embed, model=ST),
-        batched=True,
-        batch_size=16
+        features=new_features
     )
-    chunks_dataset.add_faiss_index("embeddings")
     chunks_dataset.to_json("embedding_chunks.json")
+    chunks_dataset.add_faiss_index("embeddings")
+    return chunks_dataset
+
+
+def load_embedding_index(file):
+    chunks_dataset = Dataset.from_json(file)
+    chunks_dataset.add_faiss_index("embeddings")
     return chunks_dataset
 
 
@@ -50,8 +63,9 @@ def search(query: str, model: SentenceTransformer, dataset: Dataset, k: int = 3 
     """a function that embeds a new query and returns the most probable results"""
     embedded_query = model.encode(query) # embed new query
     scores, retrieved_examples = dataset.get_nearest_examples( # retrieve results
-        "embeddings", embedded_query, # compare our new embedded query with the dataset embeddings
-        k=k # get only top k results
+        "embeddings", 
+        embedded_query, # compare our new embedded query with the dataset embeddings
+        k # get only top k results
     )
     return scores, retrieved_examples
 
@@ -84,7 +98,10 @@ def main(**kwargs):
         print("WARNING: Resizing the embedding matrix to match the tokenizer vocab size.")
         model.resize_token_embeddings(len(tokenizer))
     ST = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
-    index_dateset = create_index(ST)
+    if kwargs["embedding_index"]:
+        index_dateset = load_embedding_index(kwargs["embedding_index"])
+    else:
+        index_dateset = create_embedding_index(ST)
 
     # c_n_r_chunks = c_n_r_chunks[:int(len(c_n_r_chunks)*0.001)]
     evaluation(model=model, eval_data=c_n_r_chunks_eval, tokenizer=tokenizer, index=index_dateset, ST=ST)
@@ -103,24 +120,25 @@ def evaluation(model, eval_data, tokenizer, index, ST):
     Returns: Accuracy, CodeBLEU
     """
     os.makedirs("results", exist_ok=True)
+    codebleu_score = []
     for id, batch in enumerate(tqdm(eval_data,colour="green", desc="evaluating Epoch", dynamic_ncols=True)):
         SYS_PROMPT = """You are an assistant for resolving merge conflicts with given conflict chunks.
         You are first given several resolution examples following with an unresolved conflict chunk. Provide the resolution of the conflict chunk.
         Only output the resolution and do not output any explanation."""
         prompt = ""
         _ , retrieved_chunks = search(batch['conflict'], ST, index,  5)
-        for i, chunk in enumerate(retrieved_chunks):
+        for i, (conflict, resolution) in enumerate(zip(retrieved_chunks['conflict'], retrieved_chunks['resolution'])):
             prompt += (f"""
 Resolved Conflict {i}:
 
 ```
-{chunk['conflict']}
+{conflict}
 ```
 
 Resolution:
 
 ```
-{chunk['resolution']}
+{resolution}
 ```
 
 """)
@@ -141,7 +159,6 @@ Resolution:
             add_generation_prompt=True,
             return_tensors="pt"
             ).to(model.device)
-        codebleu_score = []
 
         start = time.perf_counter()
         with torch.no_grad():
@@ -160,16 +177,17 @@ Resolution:
         print(f"the inference time is {e2e_inference_time} ms")
         # Only get the output text without the prompt text
         # output_text = tokenizer.decode(outputs[0][torch.sum(input_ids['attention_mask']).item()+1:], skip_special_tokens=True)
+        output_text_full: str = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
         output_text: str = tokenizer.batch_decode(outputs[:, input_ids.shape[1]:], skip_special_tokens=True)[0]
         resolution = output_text.split('\n')
         if len(resolution) > 2:
             resolution = '\n'.join(resolution[1:-1])
         else:
             resolution = ''
-        codebleu_result = calc_codebleu([batch['resolution']], [resolution], lang="java", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=tokenizer)
+        codebleu_result = calc_codebleu([batch['resolution']], [resolution], lang="java", weights=(0.25, 0.25, 0.25, 0.25))
         codebleu_score.append(codebleu_result['codebleu'])
-        with open(f"results/{id}.txt", "w") as f:
-            f.write(f"######Resolution output:\n{resolution}\n######Reference resolution\n{batch['resolution']}\n######codebleu result:\n{codebleu_result}")
+        with open(f"results/{id}_{codebleu_result['codebleu']}.txt", "w") as f:
+            f.write(f"######Full output:\n{output_text_full}\n######Resolution output:\n{resolution}\n######Reference resolution\n{batch['resolution']}\n######codebleu result:\n{codebleu_result}")
         print(codebleu_result['codebleu'])
         
     with open(f"results/average_codebleu.txt", "w") as f:
